94a0f12b653503ed6bfd3389b0e13e7355a04b57
TR <atheologidesrodriguez@gmail.com>
2020-11-27 08:41:26 -0500

    Added all scenes and script framework

---
+++ NewBladePoint/Assets/Scripts/DuelistController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DuelistController : MonoBehaviour
+{
+    public bool left;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            
+        }
+
+        if (left && Input.GetKey(KeyCode.RightArrow))
+        {
+            
+        }
+
+        if (!left && Input.GetKey(KeyCode.LeftArrow))
+        {
+            
+        }
+    }
+}
