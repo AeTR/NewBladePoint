9c7cccd2e6902d8e80d39d2fb0661d7e9b613fda
Para!! <pianoducks.paradox@gmail.com>
2020-11-28 15:29:46 -0600

    Added Clouds for lvl 1 (backgrounds folder, steph's test scenes, lvl1)

---
+++ NewBladePoint/Assets/Scripts/CloudMove.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CloudMove : MonoBehaviour
+{
+    public GameObject[] clouds;
+    public float dist;
+    public float maxDist;
+    public float moveSpd;
+    
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        for (int i = 0; i < clouds.Length; i++)
+        {
+            if (clouds[i].transform.position.x>=maxDist)
+            {
+                if (i<clouds.Length-1)
+                {
+                    clouds[i].transform.position = new Vector3(clouds[i+1].transform.position.x-dist,0, 2);
+                }
+                else
+                {
+                    clouds[i].transform.position = new Vector3(clouds[0].transform.position.x-dist,0, 2);
+                }
+            }
+            clouds[i].transform.position += new Vector3(moveSpd*Time.deltaTime, 0, 0);
+        }
+        
+    }
+}
