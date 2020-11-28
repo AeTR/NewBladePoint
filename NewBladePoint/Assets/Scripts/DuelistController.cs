96f663142437d115de173f52c7817c64fc6ecbdd
Alex Theologides Rodriguez <atheologidesrodriguez@gmail.com>
2020-11-28 14:05:27 -0500

    fixed the gitignore, pushing relevant stuff for Jordo

--- NewBladePoint/Assets/Scripts/DuelistController.cs
+++ NewBladePoint/Assets/Scripts/DuelistController.cs
@@ -4,31 +4,66 @@ using UnityEngine;
 
 public class DuelistController : MonoBehaviour
 {
+    public SpriteRenderer mySpriteRenderer;
+    public Animator myAnimator;
+    public Animator leftAnim, rightAnim; //if necessary
     public bool left;
+
+    public enum DuelistState
+    {
+        Idle,
+        Charging,
+        Blocking
+    }
+
+    public DuelistState myState;
     // Start is called before the first frame update
     void Start()
     {
-        
+        left = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space))
         {
-            
+            myAnimator.SetTrigger("Stab");
+            //set the thing
         }
 
-        if (left && Input.GetKey(KeyCode.RightArrow))
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            myAnimator.SetTrigger("Fire");
+        }
+
+        if (left && Input.GetKeyDown(KeyCode.RightArrow))
         {
             left = false;
             //animation plays and location/orientation changes
         }
 
-        if (!left && Input.GetKey(KeyCode.LeftArrow))
+        if (!left && Input.GetKeyDown(KeyCode.LeftArrow))
         {
             left = true;
             //animation plays and location/orientation changes
         }
+    }
+    
+    //currently, all of these simply play their animations
+
+    public void Stab()
+    {
+        myAnimator.SetTrigger("Stab");
+    }
+
+    public void FireBreath()
+    {
+        myAnimator.SetTrigger("Fire");
+    }
+
+    public void Block()
+    {
+        myAnimator.SetTrigger("Block");
     }
 }
---
+++ NewBladePoint/Assets/Scripts/Player.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public DuelistController leftDuelist, rightDuelist, currentDuelist;
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentDuelist = rightDuelist;
+        leftDuelist.gameObject.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (currentDuelist == rightDuelist && Input.GetKeyDown(KeyCode.LeftArrow) && currentDuelist.myState == DuelistController.DuelistState.Idle)
+        {
+            //switch to left
+            leftDuelist.gameObject.SetActive(true);
+            currentDuelist = leftDuelist;
+            rightDuelist.gameObject.SetActive(false);
+        }
+
+        if (currentDuelist == leftDuelist && Input.GetKeyDown(KeyCode.RightArrow) && currentDuelist.myState == DuelistController.DuelistState.Idle)
+        {
+            //switch to right
+            rightDuelist.gameObject.SetActive(true);
+            currentDuelist = rightDuelist;
+            leftDuelist.gameObject.SetActive(false);
+        }
+
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            currentDuelist.FireBreath();
+        }
+
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            currentDuelist.Block();
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            //Finn did the press/hold thing in an interesting way, I haven't put it in yet.
+            currentDuelist.Stab();
+        }
+        
+        
+    }
+}
